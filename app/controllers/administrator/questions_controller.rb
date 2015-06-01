fba544ae4e0c81e1b0e4a3ad0a9de42f9ddfbfa3
dima-antonenko <d.antonenko94@mail.ru>
2015-06-01 20:35:55 +0300

    add new styles

---
+++ app/controllers/administrator/questions_controller.rb
@@ -0,0 +1,38 @@
+class Administrator::QuestionsController < AdministratorController
+
+  before_action :set_question, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @questions = Question.all
+  end 
+
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  # PATCH/PUT /questions/1
+  # PATCH/PUT /questions/1.jso
+
+  def destroy
+    @question.destroy
+    respond_to do |format|
+      format.html { redirect_to '/administrator/questions', notice: 'Вопрос удален' }
+      format.json { head :no_content }
+    end
+  end
+
+  # DELETE /questions/1
+  # DELETE /questions/1.jso
+
+  private
+
+  def question_params
+    params.require(:question)
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
+  end
+
+end
