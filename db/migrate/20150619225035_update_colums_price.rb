fd5c3eb5309f4b87682c2bf8cdb033a005312086
dima-antonenko <d.antonenko94@mail.ru>
2015-07-23 10:38:31 +0300

    update

--- db/migrate/20150619225035_update_colums_price.rb
+++ db/migrate/20150619225035_update_colums_price.rb
@@ -1,7 +1,6 @@
 class UpdateColumsPrice < ActiveRecord::Migration
   def change
-  	remove_column :projects, :start_pice, :string
-  	remove_column :projects, :final_pice, :string
+  	
 
   	add_column :projects, :start_pice, :integer, default: 0, index: true
   	add_column :projects, :final_pice, :integer, default: 0, index: true
