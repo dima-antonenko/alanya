3dad7ed5fd85f0bbe0fd96437a4f380c258616f0
dima-antonenko <d.antonenko94@mail.ru>
2015-08-26 08:20:49 +0300

    update

--- db/migrate/20150824075718_remove_old_columns.rb
+++ db/migrate/20150824075718_remove_old_columns.rb
@@ -1,6 +1,6 @@
 class RemoveOldColumns < ActiveRecord::Migration
   def change
-  	remove_column :projects, :final_price
+  	#remove_column :projects, :final_pice
   	remove_column :projects, :final_price
   	add_column :projects, :final_price, :integer, index: true
   end
